2f620d9b84efe88441f92c8ec961741fb9e39c48
Lakshmikanta <frontendfixer@gmail.com>
2023-03-04 22:07:44 +0530

    null-ls: add more formatter and format on save feature

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -3,17 +3,55 @@ if not null_ls_status_ok then
 	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local null_ls = require("null-ls")
+
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 
+local sources = {
+	--[[ formatting ]]
+	formatting.eslint_d,
+	formatting.stylua,
+	formatting.clang_format,
+	formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote", "--arrow_parens" } }),
+	formatting.black.with({ extra_args = { "--fast" } }),
+	formatting.beautysh,
+
+	--[[ code actions ]]
+	code_actions.eslint_d,
+
+	--[[ diagnostics ]]
+	diagnostics.eslint_d,
+}
+
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
+end
+
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    -- diagnostics.flake8
-	},
+	sources = sources,
+	on_attach = on_attach,
 })
