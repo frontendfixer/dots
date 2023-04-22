326c8a504520ba6c0c830b09eab08d40542c4bde
Lakshmikanta <frontendfixer@github.com>
2023-04-22 09:13:57 +0530

    formatting

--- .scripts/redshift.sh
+++ .scripts/redshift.sh
@@ -4,44 +4,44 @@ envFile=~/.scripts/redshift/env.sh
 changeValue=300
 
 changeMode() {
-  sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile 
-  REDSHIFT=$2
-  echo $REDSHIFT
+    sed -i "s/REDSHIFT=$1/REDSHIFT=$2/g" $envFile
+    REDSHIFT=$2
+    echo $REDSHIFT
 }
 
 changeTemp() {
-  if [ "$2" -gt 1000 ] && [ "$2" -lt 25000 ]
-  then
-    sed -i "s/REDSHIFT_TEMP=$1/REDSHIFT_TEMP=$2/g" $envFile 
-    redshift -P -O $((REDSHIFT_TEMP+changeValue))
-  fi
+    if [ "$2" -gt 1000 ] && [ "$2" -lt 25000 ]
+    then
+        sed -i "s/REDSHIFT_TEMP=$1/REDSHIFT_TEMP=$2/g" $envFile
+        redshift -P -O $((REDSHIFT_TEMP+changeValue))
+    fi
 }
 
-case $1 in 
-  toggle) 
-    if [ "$REDSHIFT" = on ];
-    then
-      changeMode "$REDSHIFT" off
-      redshift -x
-    else
-      changeMode "$REDSHIFT" on
-      redshift -O "$REDSHIFT_TEMP"
-    fi
-    ;;
-  increase)
-    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
-    ;;
-  decrease)
-    changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP-changeValue));
-    ;;
-  temperature)
-    case $REDSHIFT in
-      on)
-        printf "%dK" "$REDSHIFT_TEMP"
+case $1 in
+    toggle)
+        if [ "$REDSHIFT" = on ];
+        then
+            changeMode "$REDSHIFT" off
+            redshift -x
+        else
+            changeMode "$REDSHIFT" on
+            redshift -O "$REDSHIFT_TEMP"
+        fi
         ;;
-      off)
-        printf "off"
+    increase)
+        changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP+changeValue))
         ;;
-    esac
-    ;;
+    decrease)
+        changeTemp $((REDSHIFT_TEMP)) $((REDSHIFT_TEMP-changeValue));
+        ;;
+    temperature)
+        case $REDSHIFT in
+            on)
+                printf "%dK" "$REDSHIFT_TEMP"
+                ;;
+            off)
+                printf "off"
+                ;;
+        esac
+        ;;
 esac
