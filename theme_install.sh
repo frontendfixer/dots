5e7b782bf00c2b47136fde208373f56542d154a6
Lakshmikanta Patra <frontendfixer@gmail.com>
2024-07-03 07:12:45 +0530

    fix: themes not installing

--- theme_install.sh
+++ theme_install.sh
@@ -21,11 +21,11 @@ cd themes
 echo -e "${Y}+#+#+#+#+#+#+#+#+#+# Installing cursor icons +#+#+#+#+#+#+#+#+#+#${N}"
 curl -OL https://github.com/dracula/gtk/releases/latest/download/Dracula-cursors.tar.xz
 tar -xf Dracula-cursors.tar.xz --directory=/usr/share/icons
-curl -OL https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice-Right.tar.xz
-tar -xf Bibata-Modern-Ice-Right.tar.xz --directory=/usr/share/icons
+curl -OL https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
+tar -xf Bibata-Modern-Ice.tar.xz --directory=/usr/share/icons
 
 echo -e "${Y}+#+#+#+#+#+#+#+#+#+# Installing theme +#+#+#+#+#+#+#+#+#+#${N}"
 curl -OL https://github.com/dracula/gtk/releases/latest/download/Dracula.tar.xz
 tar -xf Dracula.tar.xz --directory=/usr/share/themes
 curl -OL https://github.com/EliverLara/Nordic/releases/latest/download/Nordic-darker.tar.xz
-tar -xf Nordic-darker.xz --directory=/usr/share/themes
+tar -xf Nordic-darker.tar.xz --directory=/usr/share/themes
