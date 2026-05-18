522dbfea4e6fff25c975a5997bb7abbb15d87e48
Lakshmikanta Patra <33117526+frontendfixer@users.noreply.github.com>
2026-05-18 20:25:18 +0530

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,16 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-feh --bg-fill --randomize /usr/share/backgrounds/fantacy/* &
+feh --bg-fill --randomize /usr/share/backgrounds/fantasy/* &
 picom -b --config ~/.config/picom/picom.conf &
 xscreensaver &
 blueman-applet &
 nm-applet &
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-#### for fedora
-#/usr/libexec/polkit-gnome-authentication-agent-1 &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-numlockx on &
-clipit &
+
+# Polkit agent (Fedora / Debian paths)
+polkit_paths=(
+    "/usr/libexec/polkit-gnome-authentication-agent-1"
+    "/usr/libexec/polkit-mate-authentication-agent-1"
+    "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+    "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
+)
+for agent in "${polkit_paths[@]}"; do
+    if [ -x "$agent" ]; then
+        "$agent" &
+        break
+    fi
+done
+
+command -v numlockx >/dev/null && numlockx on
+command -v clipman >/dev/null && clipman &
+command -v clipit >/dev/null && clipit &
 dunst &
 unclutter -root &
 ~/.scripts/redshift-on &
